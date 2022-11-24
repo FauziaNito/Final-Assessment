f4d863e7fbe93642df854e6b937a347136325d3b
Fauzia Nakacwa <fauzianaava@gmail.com>
2022-11-24 20:25:25 +0300

    Add server file dabase configurations

--- Fauzia-Nakacwa/server.js
+++ Fauzia-Nakacwa/server.js
@@ -3,11 +3,25 @@ const express = require("express");
 const session = require("express-session")
 const path = require("path");
 const mongoose = require("mongoose");
+const config = require('./config/db');
 
 
 
 // Express Instance/instantiations
 const app = express();
+
+// Database Connection
+mongoose.connect(config.database, {useNewUrlParser: true});
+const db = mongoose.connection;
+
+// Check for connection
+db.once('open', function(){
+    console.log('Made a connection Again');
+});
+// Check for Error
+db.on('error', function(err){
+    console.error(err);
+});
 
 // Configurations
 app.set('view engine', 'pug');
